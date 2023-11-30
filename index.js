f6e5af12212986511ecc2aff9b4ba04e92365fe1
shiyam-sarker10 <kaneki438500929@gmail.com>
2023-11-30 18:16:34 +0600

    added volunteer dashboard functionality

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
 
     // collections---------
@@ -458,12 +458,19 @@ async function run() {
       res.send(result);
     });
 
+    // get funds 
+    
+    app.get("/funds", async (req, res) => {
+      const result = await FundCollection.find().toArray();
+      res.send(result);
+    });
+
     
 
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
